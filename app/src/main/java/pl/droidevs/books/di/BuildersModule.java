62c95ecc53b0a1d8c77f7fbced4d2c380335f624
Karol Lisiewicz <karol.lisiewicz@gmail.com>
2017-11-20 21:29:02 +0100

    Basic dagger configuration.

---
+++ app/src/main/java/pl/droidevs/books/di/BuildersModule.java
@@ -0,0 +1,20 @@
+package pl.droidevs.books.di;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+import pl.droidevs.books.library.LibraryActivity;
+import pl.droidevs.books.login.LoginActivity;
+
+/**
+ * Module for all sub-components within the app.
+ */
+@Module
+public abstract class BuildersModule {
+    @ContributesAndroidInjector
+    abstract LibraryActivity bindLibraryActivity();
+
+    @ContributesAndroidInjector
+    abstract LoginActivity bindLoginActivity();
+
+    // Bindings for other sub-components belong here
+}
