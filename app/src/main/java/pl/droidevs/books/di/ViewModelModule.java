1fce46e3960babd0fd50f653af0221b94c16b0c7
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-20 19:30:51 +0100

    di for new viewmodel

--- app/src/main/java/pl/droidevs/books/di/ViewModelModule.java
+++ app/src/main/java/pl/droidevs/books/di/ViewModelModule.java
@@ -13,6 +13,7 @@ import dagger.Binds;
 import dagger.MapKey;
 import dagger.Module;
 import dagger.multibindings.IntoMap;
+import pl.droidevs.books.library.ExportImportViewModel;
 import pl.droidevs.books.library.LibraryViewModel;
 import pl.droidevs.books.login.LoginViewModel;
 
@@ -36,6 +37,11 @@ public abstract class ViewModelModule {
     @Binds
     @ViewModelKey(LoginViewModel.class)
     abstract ViewModel loginViewModel(LoginViewModel viewModel);
+
+    @IntoMap
+    @Binds
+    @ViewModelKey(ExportImportViewModel.class)
+    abstract ViewModel exportImportViewModel(ExportImportViewModel viewModel);
 
     @Binds
     abstract ViewModelProvider.Factory viewModelFactory(ViewModelFactory factory);
