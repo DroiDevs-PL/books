865dfee8dd9c01fddf461ffafbf6f35c1b623fcf
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-23 12:45:12 +0100

    creating new file

--- app/src/main/java/pl/droidevs/books/library/ExportImportViewModel.java
+++ app/src/main/java/pl/droidevs/books/library/ExportImportViewModel.java
@@ -1,6 +1,9 @@
 package pl.droidevs.books.library;
 
 import android.arch.lifecycle.ViewModel;
+import android.os.Environment;
+
+import java.io.File;
 
 import javax.inject.Inject;
 
@@ -8,6 +11,7 @@ import pl.droidevs.books.repository.BookRepository;
 
 public class ExportImportViewModel extends ViewModel {
 
+    private static final String FILE_NAME = "Books.csv";
     private BookRepository bookRepository;
 
     @Inject
@@ -16,5 +20,10 @@ public class ExportImportViewModel extends ViewModel {
     }
 
     public void exportBooks() {
+        String baseDirectoryPath = Environment
+                .getExternalStorageDirectory()
+                .getAbsolutePath();
+        String filePath = baseDirectoryPath + File.separator + FILE_NAME;
+        File file = new File(filePath);
     }
 }
