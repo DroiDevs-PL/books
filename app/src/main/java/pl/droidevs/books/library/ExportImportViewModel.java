7305c676f51e91f69b66579f01ce29644809e11a
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-25 21:29:31 +0100

    improved exportimportviewmodel

--- app/src/main/java/pl/droidevs/books/library/ExportImportViewModel.java
+++ app/src/main/java/pl/droidevs/books/library/ExportImportViewModel.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.os.Environment;
 import android.support.annotation.Nullable;
+
 import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
@@ -28,20 +29,36 @@ public class ExportImportViewModel extends ViewModel {
         this.bookCsvRepository = bookRepository;
     }
 
-    public void exportBooks() {
-        File file = getFile();
-        LiveData<List<BookEntity>> booksLiveData = this.bookCsvRepository.getBookEntities();
+    public boolean exportBooks() {
+        File file;
 
+        try {
+            file = getFile();
+        } catch (IOException e) {
+            return false;
+        }
+
+        LiveData<List<BookEntity>> booksLiveData = this.bookCsvRepository.getBookEntities();
+        final boolean[] wasSavingSuccessful = {true};
         booksLiveData.observeForever(new Observer<List<BookEntity>>() {
 
             @Override
             public void onChanged(@Nullable List<BookEntity> books) {
-                writeToFile(file, books);
-                booksLiveData.removeObserver(this);
-            }});
+
+                try {
+                    writeToFile(file, books);
+                } catch (IOException e) {
+                    wasSavingSuccessful[0] = false;
+                } finally {
+                    booksLiveData.removeObserver(this);
+                }
+            }
+        });
+
+        return wasSavingSuccessful[0];
     }
 
-    private File getFile() {
+    private File getFile() throws IOException {
         String baseDirectoryPath = Environment
                 .getExternalStorageDirectory()
                 .getAbsolutePath();
@@ -49,26 +66,22 @@ public class ExportImportViewModel extends ViewModel {
 
         File file = new File(filePath);
 
-        try {
-
-            if (!file.exists()) {
-                file.createNewFile();
-            } else {
-                file.delete();
-                file.createNewFile();
-            }
-
-            return file;
-        } catch (IOException e) {
-            return null;
+        if (!file.exists()) {
+            file.createNewFile();
+        } else {
+            file.delete();
+            file.createNewFile();
         }
+
+        return file;
     }
 
-    private void writeToFile(File file, List<BookEntity> books) {
+    private void writeToFile(File file, List<BookEntity> books) throws IOException {
+        CsvBeanWriter csvBeanWriter = null;
 
         try {
             FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
-            CsvBeanWriter csvBeanWriter = new CsvBeanWriter(fileWriter, CsvPreference.STANDARD_PREFERENCE);
+            csvBeanWriter = new CsvBeanWriter(fileWriter, CsvPreference.STANDARD_PREFERENCE);
 
             csvBeanWriter.write(CSVHelper.getBookEntityCsvHeaders());
 
@@ -76,9 +89,11 @@ public class ExportImportViewModel extends ViewModel {
                 csvBeanWriter.write(bookEntity, CSVHelper.getBookEntityCsvHeaders());
             }
 
-            csvBeanWriter.close();
-        } catch (IOException e) {
+        } finally {
 
+            if (csvBeanWriter != null) {
+                csvBeanWriter.close();
+            }
         }
     }
 }
