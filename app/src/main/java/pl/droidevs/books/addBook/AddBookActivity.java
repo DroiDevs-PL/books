f86e778fd6c88a47ff1dca154ed8e660d0bd6a40
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-08 15:45:27 +0100

    created layout for activity

--- app/src/main/java/pl/droidevs/books/addBook/AddBookActivity.java
+++ app/src/main/java/pl/droidevs/books/addBook/AddBookActivity.java
@@ -1,6 +1,17 @@
 package pl.droidevs.books.addBook;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import pl.droidevs.books.R;
+
 public class AddBookActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_add_book);
+    }
 }
