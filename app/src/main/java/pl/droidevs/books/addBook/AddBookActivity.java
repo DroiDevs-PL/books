20b12788ad8b174ae0d01ba3464e996a17c2fc25
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-08 15:53:21 +0100

    setting up view model in addbookactivity

--- app/src/main/java/pl/droidevs/books/addBook/AddBookActivity.java
+++ app/src/main/java/pl/droidevs/books/addBook/AddBookActivity.java
@@ -1,17 +1,30 @@
 package pl.droidevs.books.addBook;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import javax.inject.Inject;
+
 import pl.droidevs.books.R;
 
 public class AddBookActivity extends AppCompatActivity {
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_add_book);
+
+        setupViewModel();
+    }
+
+    private void setupViewModel() {
+        final AddBookViewModel addBookViewModel = ViewModelProviders.of(this, viewModelFactory).get(AddBookViewModel.class);
     }
 }
