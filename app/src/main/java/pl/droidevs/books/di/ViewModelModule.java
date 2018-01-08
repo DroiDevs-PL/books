f0c34d702dba363a685667aedbc65573d4a165b0
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-08 15:47:46 +0100

    added binding for addbookviewmodel

--- app/src/main/java/pl/droidevs/books/di/ViewModelModule.java
+++ app/src/main/java/pl/droidevs/books/di/ViewModelModule.java
@@ -13,6 +13,7 @@ import dagger.Binds;
 import dagger.MapKey;
 import dagger.Module;
 import dagger.multibindings.IntoMap;
+import pl.droidevs.books.addBook.AddBookViewModel;
 import pl.droidevs.books.exportimport.ExportImportViewModel;
 import pl.droidevs.books.library.LibraryViewModel;
 import pl.droidevs.books.login.LoginViewModel;
@@ -42,6 +43,11 @@ public abstract class ViewModelModule {
     @Binds
     @ViewModelKey(ExportImportViewModel.class)
     abstract ViewModel exportImportViewModel(ExportImportViewModel viewModel);
+
+    @IntoMap
+    @Binds
+    @ViewModelKey(AddBookViewModel.class)
+    abstract ViewModel addBookViewModel(AddBookViewModel viewModel);
 
     @Binds
     abstract ViewModelProvider.Factory viewModelFactory(ViewModelFactory factory);
