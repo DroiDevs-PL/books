c648750880b4602125d195a541c60a414a08984c
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-08 16:40:02 +0100

    added butterknife for addingbookactivity

--- app/src/main/java/pl/droidevs/books/addBook/AddBookActivity.java
+++ app/src/main/java/pl/droidevs/books/addBook/AddBookActivity.java
@@ -6,12 +6,24 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
+import dagger.android.AndroidInjection;
 import pl.droidevs.books.R;
 
 public class AddBookActivity extends AppCompatActivity {
+
+    @BindView(R.id.coverImageUrlEditText)
+    EditText coverUrlEditText;
+
+    @BindView(R.id.coverImageView)
+    ImageView coverImageView;
 
     @Inject
     ViewModelProvider.Factory viewModelFactory;
@@ -21,10 +33,18 @@ public class AddBookActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_add_book);
 
+        AndroidInjection.inject(this);
+        ButterKnife.bind(this);
+
         setupViewModel();
     }
 
     private void setupViewModel() {
         final AddBookViewModel addBookViewModel = ViewModelProviders.of(this, viewModelFactory).get(AddBookViewModel.class);
+    }
+
+    @OnTextChanged(R.id.coverImageUrlEditText)
+    void onCoverUrlChanged() {
+        
     }
 }
