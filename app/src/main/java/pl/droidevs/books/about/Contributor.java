8719ae667ce4719334752d70e56953c34213d004
Michał Bachta <michal.bachta@outlook.com>
2018-03-17 22:30:03 +0100

    Contibutor UI

--- app/src/main/java/pl/droidevs/books/about/Contributor.java
+++ app/src/main/java/pl/droidevs/books/about/Contributor.java
@@ -1,5 +1,7 @@
 package pl.droidevs.books.about;
 
+import android.support.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,14 +16,21 @@ public class Contributor {
     private final String name;
     private final String nick;
     private final int resourceImage;
-    private final String gitHubUrl;
+    private final String githubUrl;
+    private final String linkedInUrl;
+    private final String googlePlayStoreUrl;
+    private final String websiteUrl;
 
 
-    public Contributor(String name, String nick, int resourceImage, String gitHubUrl) {
+    private Contributor(String name, String nick, int resourceImage, String githubUrl,
+                       @Nullable String linkedInUrl, @Nullable String googlePlayStoreUrl, @Nullable String websiteUrl) {
         this.name = name;
         this.nick = nick;
         this.resourceImage = resourceImage;
-        this.gitHubUrl = gitHubUrl;
+        this.githubUrl = githubUrl;
+        this.linkedInUrl = linkedInUrl;
+        this.googlePlayStoreUrl = googlePlayStoreUrl;
+        this.websiteUrl = websiteUrl;
     }
 
     public String getName() {
@@ -36,8 +45,23 @@ public class Contributor {
         return resourceImage;
     }
 
-    public String getGitHubUrl() {
-        return gitHubUrl;
+    public String getGithubUrl() {
+        return githubUrl;
+    }
+
+    @Nullable
+    public String getLinkedInUrl() {
+        return linkedInUrl;
+    }
+
+    @Nullable
+    public String getGooglePlayStoreUrl() {
+        return googlePlayStoreUrl;
+    }
+
+    @Nullable
+    public String getWebsiteUrl() {
+        return websiteUrl;
     }
 
 
@@ -47,22 +71,34 @@ public class Contributor {
         team.add(new Contributor("Natalia Jastrzębska",
                 "nani92",
                 R.drawable.contributor_nani,
-                "https://github.com/nani92"));
+                "https://github.com/nani92",
+                null,
+                null,
+                null));
 
         team.add(new Contributor("Karol Lisiewicz",
                 "klisiewicz",
                 R.drawable.contributor_klisiewicz,
-                "https://github.com/klisiewicz"));
+                "https://github.com/klisiewicz",
+                null,
+                null,
+                null));
 
         team.add(new Contributor("Michał Bachta",
                 "MichaelB-pl",
                 R.drawable.contributor_mb,
-                "https://github.com/MichaelB-pl"));
+                "https://github.com/MichaelB-pl",
+                "https://www.linkedin.com/in/micha%C5%82-bachta/",
+                null,
+                null));
 
         team.add(new Contributor("Adam Świderski",
                 "asvid",
                 R.drawable.contributor_asvid,
-                "https://github.com/asvid"));
+                "https://github.com/asvid",
+                null,
+                null,
+                null));
 
         return team;
     }
--- app/src/main/java/pl/droidevs/books/about/ContributorFragment.java
+++ app/src/main/java/pl/droidevs/books/about/ContributorFragment.java
@@ -1,5 +1,7 @@
 package pl.droidevs.books.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -37,6 +40,27 @@ public class ContributorFragment extends Fragment {
     @BindView(R.id.tv_github)
     TextView tvGithub;
 
+    @BindView(R.id.tv_linkedin)
+    TextView tvLinkedIn;
+
+    @BindView(R.id.tv_google_play_store)
+    TextView tvGooglePlayStore;
+
+    @BindView(R.id.tv_www)
+    TextView tvWww;
+
+    @BindView(R.id.ll_github)
+    LinearLayout llGitHub;
+
+    @BindView(R.id.ll_linkedin)
+    LinearLayout llLinkedIn;
+
+    @BindView(R.id.ll_google_play_store)
+    LinearLayout llGooglePlayStore;
+
+    @BindView(R.id.ll_www)
+    LinearLayout llWww;
+
     public static ContributorFragment newInstance(int contributorIndex) {
         ContributorFragment fragment = new ContributorFragment();
         Bundle args = new Bundle();
@@ -55,9 +79,7 @@ public class ContributorFragment extends Fragment {
         if (args != null) {
             int contributorIndex = args.getInt(CONTRIBUTOR_INDEX);
             Contributor contributor = Contributor.getTeam().get(contributorIndex);
-            tvName.setText(contributor.getName());
-            tvGithub.setText(contributor.getNick());
-            Glide.with(this).load(contributor.getResourceImage()).into(ivAvatar);
+            setData(contributor);
         }
 
         return view;
@@ -82,4 +104,43 @@ public class ContributorFragment extends Fragment {
         setContentView(R.layout.fragment_contributor);
         ButterKnife.bind(this);
     }*/
+
+    private void setData(Contributor contributor) {
+        tvName.setText(contributor.getName());
+        tvNick.setText(contributor.getNick());
+
+        llGitHub.setOnClickListener(view -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contributor.getGithubUrl()));
+            startActivity(browserIntent);
+        });
+
+        if (contributor.getLinkedInUrl() != null) {
+            llLinkedIn.setOnClickListener(view -> {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contributor.getLinkedInUrl()));
+                startActivity(browserIntent);
+            });
+        } else {
+            llLinkedIn.setVisibility(View.GONE);
+        }
+
+        if (contributor.getGooglePlayStoreUrl() != null) {
+            llGooglePlayStore.setOnClickListener(view -> {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contributor.getGooglePlayStoreUrl()));
+                startActivity(browserIntent);
+            });
+        } else {
+            llGooglePlayStore.setVisibility(View.GONE);
+        }
+
+        if (contributor.getWebsiteUrl() != null) {
+            llWww.setOnClickListener(view -> {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contributor.getWebsiteUrl()));
+                startActivity(browserIntent);
+            });
+        } else {
+            llWww.setVisibility(View.GONE);
+        }
+
+        Glide.with(this).load(contributor.getResourceImage()).into(ivAvatar);
+    }
 }
